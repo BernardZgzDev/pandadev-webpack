10b0fc02b4cbd1be76f02c618d1377000e4c787e
BernardZgzDev <bernard.moralesn@pandadev.es>
2021-06-28 10:25:12 +0200

    Webpack y CIS-ITSM certification

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { init } from './js/component';
+import './css/component.css';
+
+init();
---
+++ src/js/component.js
@@ -0,0 +1,59 @@
+export const init = () => {
+
+    $(document).ready(function(){
+		
+		perfilToggle();
+
+		$('#aboutme').click(function () {
+			$('#toggle-icon-aboutme').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+		});
+
+		$('#contact').click(function () {
+			$('#toggle-icon-contact').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+		});
+
+		$('#skills').click(function () {
+			$('#toggle-icon-skills').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+		});
+
+		$('#myprojects').click(function () {
+			$('#toggle-icon-projects').toggleClass("fa-chevron-down").toggleClass("fa-chevron-up");
+		});
+
+		$('.skill').mouseover(function () { 
+			$(this).parent().siblings().children('p').children().animate({fontSize: '110%'}, 'fast');
+			$(this).parent().siblings().children('p').children().animate({fontSize: '100%'}, 'fast');                
+		});
+
+		$('.stars').mouseover(function () {         
+			$(this).parent().siblings().children('p').animate({fontWeight: '700'}, 'fast');
+			$(this).parent().siblings().children('p').animate({fontWeight: '400'}, 'fast');                       
+		});
+
+		$(document).on('show.bs.modal', '.modal', function () {
+			$(this).appendTo('body');
+		});
+
+	});
+
+	$(window).resize(function(){
+		perfilToggle();
+	});
+
+	function perfilToggle() {
+		var w = $(document).width();
+
+		if(w >= 752) {
+			$(".altura").addClass("vh-100");
+			$("#aboutme-details, #contact-details, #skills-details, #projects-details").addClass("show");    
+			$(".toggle-icon").addClass("fa-chevron-up");    
+			$(".toggle-icon").removeClass("fa-chevron-down");    
+			   
+		} else {
+			$(".altura").removeClass("vh-100");
+			$("#aboutme-details, #contact-details, #skills-details, #projects-details").removeClass("show"); 
+			$(".toggle-icon").addClass("fa-chevron-down");    
+			$(".toggle-icon").removeClass("fa-chevron-up");    
+		}
+	}   
+}
